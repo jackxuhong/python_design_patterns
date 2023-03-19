cd496a502e05372872b42d639a7f9adced463f89
Jack Hong <jack.xu.hong@gmail.com>
2023-03-19 18:35:03 -0400

    Updated

--- gof/AbstractFactory.py
+++ gof/AbstractFactory.py
@@ -1,42 +1,88 @@
-import json
-from decimal import Decimal
+from __future__ import annotations
+from abc import ABC, abstractmethod
 
-def build_decimal(string):
-    return Decimal(string)
+class AbstractFactory(ABC):
+    @abstractmethod
+    def create_product_a(self) -> AbstractProductA:
+        pass
 
-text = '{"total": 9.61, "items": ["Americano", "Omelet"]}'
+    @abstractmethod
+    def create_product_b(self) -> AbstractProductB:
+        pass
 
-print(json.loads(text))
-print(json.loads(text, parse_float=build_decimal))
 
-class Factory(object):
-    def build_sequence(self):
-        return []
+class ConcreteFactory1(AbstractFactory):
+    def create_product_a(self) -> AbstractProductA:
+        return ConcreteProductA1()
     
-    def build_number(self, string):
-        return Decimal(string)
-
-class Loader(object):
-    def load(string, factory):
-        sequence = factory.build_sequence()
-        for substr in string.split(','):
-            print(substr)
-            item = factory.build_number(substr)
-            sequence.append(item)
-        return sequence
-
-f = Factory()
-result = Loader.load('1.23, 4.56, 7.890', f)
-print(result)
+    def create_product_b(self) -> AbstractProductB:
+        return ConcreteProductB1()
 
 
-from abc import ABCMeta, abstractmethod
+class ConcreteFactory2(AbstractFactory):
+    def create_product_a(self) -> AbstractProductA:
+        return ConcreteProductA2()
+    
+    def create_product_b(self) -> AbstractProductB:
+        return ConcreteProductB2()
 
-class AbstractFactory(metaclass=ABCMeta):
+
+class AbstractProductA(ABC):
     @abstractmethod
-    def build_sequence(self):
+    def useful_fuction_a(self) -> str:
+        pass
+
+class ConcreteProductA1(AbstractProductA):
+    def useful_fuction_a(self) -> str:
+        return "The result of product A1."
+
+class ConcreteProductA2(AbstractProductA):
+    def useful_fuction_a(self) -> str:
+        return "The result of product A2."
+
+
+class AbstractProductB(ABC):
+    @abstractmethod
+    def func_b(self) -> None:
         pass
 
     @abstractmethod
-    def build_number(self, string):
+    def func_bb(self, collaborator: AbstractProductA) -> None:
         pass
+
+
+
+class ConcreteProductB1(AbstractProductB):
+    def func_b(self) ->  str:
+        return  "The result of product B1."
+    
+    def func_bb(self, collaborator: AbstractProductA) -> str:
+        result = collaborator.useful_fuction_a()
+        return f"The result of B1 collaborating with ({result})"
+
+
+class ConcreteProductB2(AbstractProductB):
+    def func_b(self) ->  str:
+        return  "The result of product B2."
+    
+    def func_bb(self, collaborator: AbstractProductA) -> str:
+        result = collaborator.useful_fuction_a()
+        return f"The result of B2 collaborating with ({result})"
+
+
+def client_code(factory: AbstractFactory) -> None:
+    product_a = factory.create_product_a()
+    product_b = factory.create_product_b()
+
+    print(f"{product_b.func_b()}")
+    print(f"{product_b.func_bb(product_a)}", end="")
+
+if __name__ == "__main__":
+    print("Client: testing client code wit the 1st factory type:")
+    client_code(ConcreteFactory1())
+
+    print("\n")
+
+    print("Client: testing the same client code with the 2nd factory type: ")
+    client_code(ConcreteFactory2())
+
